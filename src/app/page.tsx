e1eda552ec144c26cb55705195518bea9ae57a1e
yanrimo <yanrimo19@gmail.com>
2025-08-10 14:23:28 +0900

    feat: 🎸 initialize commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,124 @@
-import Image from "next/image";
+"use client";
+
+import { FormEvent, useState } from "react";
+
+type FoodItem = {
+  id: string;
+  name: string;
+  category: string;
+  expiryDate: string;
+  quantity: number;
+};
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [foods, setFoods] = useState<FoodItem[]>([]);
+  const [isEdit, setIsEdit] = useState<Boolean>(false);
+  const [editingId, setEditingId] = useState<FoodItem["id"]>("");
+  const [editingName, setEditingName] = useState<FoodItem["name"]>("");
+  const [editingCategory, setEditingCategory] =
+    useState<FoodItem["category"]>("");
+  const [editingQuantity, setEditingQuantity] =
+    useState<FoodItem["quantity"]>(0);
+  const [editingExpiryDate, setEditingExpiryDate] =
+    useState<FoodItem["expiryDate"]>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (isEdit) {
+      const currentFoodItems = foods.filter((food) => food.id !== editingId);
+      const newFoodItem: FoodItem = {
+        id: editingId,
+        name: editingName,
+        category: editingCategory,
+        quantity: editingQuantity,
+        expiryDate: editingExpiryDate,
+      };
+      setFoods([...currentFoodItems, newFoodItem]);
+    } else {
+      const newFoodItem: FoodItem = {
+        id: crypto.randomUUID(),
+        name: editingName,
+        category: editingCategory,
+        quantity: Number(editingQuantity),
+        expiryDate: editingExpiryDate,
+      };
+      setFoods([...foods, newFoodItem]);
+    }
+
+    resetForm();
+  };
+
+  const deleteFoodItem = (foodItemId: FoodItem["id"]) => {
+    setFoods(foods.filter((food) => food.id !== foodItemId));
+  };
+
+  const editFoodItem = (foodItem: FoodItem) => {
+    setIsEdit(true);
+    setEditingId(foodItem.id);
+    setEditingName(foodItem.name);
+    setEditingCategory(foodItem.category);
+    setEditingQuantity(foodItem.quantity);
+    setEditingExpiryDate(foodItem.expiryDate);
+  };
+
+  const resetForm = () => {
+    setEditingId("");
+    setEditingName("");
+    setEditingCategory("");
+    setEditingQuantity(0);
+    setEditingExpiryDate("");
+    setIsEdit(false);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>食材追加フォーム</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={editingName}
+            onChange={(e) => setEditingName(e.target.value)}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input
+            type="text"
+            name="category"
+            id="category"
+            value={editingCategory}
+            onChange={(e) => setEditingCategory(e.target.value)}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <input
+            type="number"
+            name="quantity"
+            id="quantity"
+            value={editingQuantity}
+            onChange={(e) => setEditingQuantity(Number(e.target.value))}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <input
+            type="date"
+            name="expiryDate"
+            id="expiryDate"
+            value={editingExpiryDate}
+            onChange={(e) => setEditingExpiryDate(e.target.value)}
+          />
+          <button type="submit">追加</button>
+        </form>
+      </div>
+      <div>
+        <h2>食材一覧</h2>
+        <ul>
+          {foods.map((food) => (
+            <li key={food.id}>
+              <p>{food.name}</p>
+              <button onClick={() => editFoodItem(food)}>edit</button>
+              <button onClick={() => deleteFoodItem(food.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
